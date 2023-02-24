6cd50b6553eba18ae9991ca5b52adc6890af34d6
alekspanteli <alekspanteli@rambler.ru>
2023-02-24 23:12:33 +0000

    map movies

--- app/page.js
+++ app/page.js
@@ -1,12 +1,27 @@
 export const metadata = {
-  title: 'Home',
-  description: 'Home page goes here',
-}
+  title: "Home",
+  description: "Home page goes here",
+};
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+
+  const res = await data.json();
+
   return (
     <main>
       <h1 className="text-lg">Hello next 13</h1>
+      <div className="grid grid-cols-4 gap-4">
+      {res.results.map((movie) => (
+        
+          <article className="shadow p-8">
+            <h1>{movie.title}</h1>
+          </article>
+        
+      ))}
+      </div>
     </main>
   );
 }
