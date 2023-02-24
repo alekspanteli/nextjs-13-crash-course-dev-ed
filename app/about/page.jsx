87d8001b81ac519618179eaae852f9ca0c890ea9
alekspanteli <alekspanteli@rambler.ru>
2023-02-24 21:39:25 +0000

    add pages

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "About",
+  description: "About page goes here",
+};
+
+export default function About() {
+  return (
+    <main>
+      <h1>About next 13</h1>
+    </main>
+  );
+}
